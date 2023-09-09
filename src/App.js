22c2de3b6c42a8f550574e7fbacb9b3448f83c3d
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-09-09 14:43:15 +0530

    Update dependencies, add new features, and fix bugs.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import BackToTop from "./components/BackToTop";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <h1 class="text-3xl font-bold underline">
+            Hello world!
+          </h1>
+        </div>
+        <Routes>
+          <Route path="/" element={<Homepage />} exact />
+        </Routes>
+        <div>
+          <BackToTop />
+          <Footer />
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
